0a2d568941bd461b6bce1068d605aa99d0ed7ba0
23f3000434 <ashuathu93@gmail.com>
2026-01-04 16:55:32 +0530

    data saving in json file

--- grades.py
+++ grades.py
@@ -1,26 +1,53 @@
-dic = {"ashu" : 39, "deepesh": 40, "monish": 31}
+import json
 
+
+def load_data():
+    try:
+        # 1. Try to open the file in 'Read' mode ('r')
+        with open('data.json', 'r') as f:
+            # 2. If file exists, load the JSON into a variable
+            data = json.load(f)
+            return data
+    except FileNotFoundError:
+        # 3. If file does NOT exist (First time running), return empty dict
+        return {}
+
+
+dic = load_data()
 
 def add_stud():
-    try:
-        s_name = input("enter name")
-        s_mark = int(input("enter marks : "))
-        if 0<= s_mark <= 100:
-            dic[s_name] = s_mark
-        else:
-            print("add marks in correct range")
-    except ValueError:
-        print("Error: Invalid Input please write in integer")
-
-
+    s_name = input("enter name: ")
+    while True:
+        try:
+            s_mark = int(input(f"Enter marks for {s_name}: "))
+            
+            if 0 <= s_mark <= 100:
+                dic[s_name] = s_mark
+                print("Saved!")
+                break  
+            else:
+                print("Error: Marks must be 0-100.") 
+                   
+        except ValueError:
+            print("Error: Please enter a number.")
+    with open('data.json', 'w') as f:
+        json.dump(dic, f)
+        
+        
 def view_stud(dic):
-    for name in dic:
-        print(f"{name} : {dic[name]}")
-    sum1 = sum(dic.values())
-    print(len(dic))
-    print(sum1)
-    avg = sum1/len(dic)
+    with open('data.json', 'r') as f:
+        data = json.load(f)
+    # print('All data', data)
+    for name in data:
+        print(f"{name} : {data[name]}")
+    sum1 = sum(data.values())
+    # print(len(dic))
+    # print(sum1)
+    avg = sum1/len(data)
     print(f"Average score: {avg} ")
+    
+
+            
 
 
 while True:
@@ -32,5 +59,8 @@ while True:
         view_stud(dic)
     elif prompt == "exit":
         break
-        
+    
+    
+    
+
     
